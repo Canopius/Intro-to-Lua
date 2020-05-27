25be98d654645fd18e550ec546be3ea2b0803e6e
Josh <65733500+Canopius@users.noreply.github.com>
2020-05-27 22:24:29 +0100

    Create FizzBuzz.lua

---
+++ FizzBuzz.lua
@@ -0,0 +1,17 @@
+--[[
+Have a go at trying to make the game Fizz Buzz in this file.
+
+Rules:
+  - Count from 1 - 100
+  - Whenever a number is divisable by 3 output 'Fizz'
+  - Whenever a number is divisable by 5 output 'Buzz'
+  - Whenever a number is divisable by both 3 and 5 output 'FizzBuzz'
+  - Else output the appropriate number
+  
+Example output:
+   1
+   2
+   Fizz
+   4
+   Buzz
+]]
